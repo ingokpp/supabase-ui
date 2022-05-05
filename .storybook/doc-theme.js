412b9ee2970a960d3f12cd8922035d66c0bfc012
Ingo Köpp <ingokpp@gmail.com>
2022-05-05 21:41:12 +0200

    Initial Commit

---
+++ .storybook/doc-theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Supabase UI',
+  brandImage: 'https://ui.supabase.io/images/logo-light.png',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import docTheme from './doc-theme';
+
+addons.setConfig({
+  theme: docTheme,
+});
