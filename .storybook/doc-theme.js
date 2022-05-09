7e9b9b6c124f50ac8ea672a8254a32863dedd213
Ingo Köpp <ingokpp@gmail.com>
2022-05-09 21:51:50 +0200

    feat: Add 'public' folder to storybook config for custom logo

--- .storybook/doc-theme.js
+++ .storybook/doc-theme.js
@@ -3,5 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'Supabase UI',
-  brandImage: 'https://ui.supabase.io/images/logo-light.png',
+  brandImage: './logo-light.png',
+  colorPrimary: "rgba(36,180,126,1)",
+  barSelectedColor: "rgba(36,180,126,1)",
 });
